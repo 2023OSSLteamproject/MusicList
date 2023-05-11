8e68999ee9c75621544aa265ac92569d913cf782
hzlloni <130718048+hzlloni@users.noreply.github.com>
2023-05-11 16:41:14 +0900

    Update mainFile List.c

--- List.c
+++ List.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(void){
+  int size = 20;
+  Music *m[20];
+  int count = 0, menu, num;
+  int del, index = 0;
+  
+  count = loadData(s);
+  index = count;
+  
+  
+  
+  return 0;
+}
