edddbe8e7427393f800f45c551e00fd45ddd37eb
hzlloni <130718048+hzlloni@users.noreply.github.com>
2023-05-18 13:20:09 +0900

    Update List.c->main

--- List.c
+++ List.c
@@ -4,6 +4,7 @@
 #include "Music.h"
 
 int main(void){
+  
   int size = 20;
   Music *m[20];
   int count = 0, menu, num;
@@ -21,20 +22,21 @@ while (1)
      break; 
     }
   else if (menu == 1 && count > 0) {
-  listScore(s, count); 
+  listMusic(s, count); 
   }
   
   else if (menu == 2 && count < size)
   {
   s[count] = (Score *)malloc(sizeof(Score)); // 동적 메모리 할당 addScore(s[count]);
+  addMusic(s[count]);
   count++;
   }
   
   else if (menu == 3 && count > 0) {
-  listScore(s, count);
+  listMusic(s, count);
   num = selectNum();
   if (num > 0 && num <= count) {
-  updateScore(s[num - 1]);
+  updateMusic(s[num - 1]);
   }
   else
   {
@@ -43,10 +45,10 @@ while (1)
   }
   
   else if (menu == 4 && count > 0) {
-  listScore(s, count);
+  listMusic(s, count);
   num = selectNum();
     if (num > 0 && num <= count) {
-    deleteScore(s, count, num);
+    deleteMusic(s, count, num);
     }
     else
     {
@@ -59,7 +61,7 @@ while (1)
   }
   
   else if (menu == 6 && count > 0){
-  searchName(s, count);
+  searchMusic(s, count);
   } 
 }
   // 동적 메모리 해제
