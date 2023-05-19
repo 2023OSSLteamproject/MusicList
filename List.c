79b74697eeb1106bfc51542b21b359b770d0d886
hzlloni <130718048+hzlloni@users.noreply.github.com>
2023-05-19 14:59:54 +0900

    Update List.c->main

--- List.c
+++ List.c
@@ -3,78 +3,84 @@
 #include <string.h>
 #include "Music.h"
 
-int main(void){
-  
-  int size = 20;
-  Music *m[20];
-  int count = 0, menu, num;
-  int del, index = 0;
-  
-  count = loadData(s);
-  index = count;
-  
-  
-while (1)
+int main(void)
 {
-    menu = selectMenu();
-    if (menu == 0)
-    {
-     break; 
-    }
-  else if (menu == 1 && count > 0) {
-    saveData(s, count); 
-  }
-  
-  else if (menu == 2 && count < size)
-  {
-    searchMusic(s, count);
 
-  }
-  
-  else if (menu == 3 && count > 0) {
-      s[count] = (Score *)malloc(sizeof(Score)); // 동적 메모리 할당 addScore(s[count]);
-      addMusic(s[count]);
-      count++;
-   
-  }
-  
-  else if (menu == 4 && count > 0) {
-    listMusic(s, count);
-    num = selectNum();
-    if (num > 0 && num <= count) {
-    deleteMusic(s, count, num);
-    }
-    else
+    int size = 20;
+    Music *m[20];
+    int count = 0, menu, num;
+    int del, index = 0;
+
+    count = loadData(m);
+    index = count;
+
+    while (1)
     {
-    printf("잘못된 번호입니다.\n");
-    } 
-  }
-  
-  else if (menu == 5 && count > 0) {
-   listMusic(s, count);
-    num = selectNum();
-    if (num > 0 && num <= count) {
-    updateMusic(s[num - 1]);
+        menu = selectMenu();
+        if (menu == 0)
+        {
+            break;
+        }
+        else if (menu == 1 && count > 0)
+        {
+            saveData(m, count);
+        }
+
+        else if (menu == 2 && count < size)
+        {
+            searchMusic(m, count);
+        }
+
+        else if (menu == 3 && count > 0)
+        {
+            m[count] = (Music *)malloc(sizeof(Music)); // 동적 메모리 할당 addScore(s[count]);
+            addMusic(m[count]);
+            count++;
+        }
+
+        else if (menu == 4 && count > 0)
+        {
+            listMusic(m, count);
+            num = selectNum();
+            if (num > 0 && num <= count)
+            {
+                deleteMusic(m, count);
+            }
+            else
+            {
+                printf("잘못된 번호입니다.\n");
+            }
+        }
+
+        else if (menu == 5 && count > 0)
+        {
+            listMusic(m, count);
+            num = selectNum();
+            if (num > 0 && num <= count)
+            {
+                updateMusic(m[num - 1]);
+            }
+            else
+            {
+                printf("잘못된 번호입니다.\n");
+            }
+        }
+
+        else if (menu == 6 && count > 0)
+        {
+            downMusic(m, count);
+        }
+        else if (menu == 7 && count > 0)
+        {
+            listenMusic(m, count);
+        }
     }
-    else
+    // 동적 메모리 해제
+    for (int i = 0; i < count; i++)
     {
-    printf("잘못된 번호입니다.\n");
-      }  
-  }
-  
-  else if (menu == 6 && count > 0){
-    downMusic(s, count);
-  } 
-   else if (menu == 7 && count > 0){
-    listenMusic(s, count);
-  } 
-}
-  // 동적 메모리 해제
-  for (int i = 0; i < count; i++) {
-      free(s[i]);
-  }
-  printf("종료됨!\n");
-  
-  
-  return 0;
+        free(m[i]);
+    }
+    printf("종료됨!\n");
+
+    return 0;
 }
