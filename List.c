cb401510c7b37c9eaa1493ec96a1b526ce034e70
hzlloni <130718048+hzlloni@users.noreply.github.com>
2023-05-19 14:54:48 +0900

    Update List.c->main

--- List.c
+++ List.c
@@ -22,31 +22,25 @@ while (1)
      break; 
     }
   else if (menu == 1 && count > 0) {
-  listMusic(s, count); 
+    saveData(s, count); 
   }
   
   else if (menu == 2 && count < size)
   {
-  s[count] = (Score *)malloc(sizeof(Score)); // 동적 메모리 할당 addScore(s[count]);
-  addMusic(s[count]);
-  count++;
+    searchMusic(s, count);
+
   }
   
   else if (menu == 3 && count > 0) {
-  listMusic(s, count);
-  num = selectNum();
-  if (num > 0 && num <= count) {
-  updateMusic(s[num - 1]);
-  }
-  else
-  {
-  printf("잘못된 번호입니다.\n");
-    } 
+      s[count] = (Score *)malloc(sizeof(Score)); // 동적 메모리 할당 addScore(s[count]);
+      addMusic(s[count]);
+      count++;
+   
   }
   
   else if (menu == 4 && count > 0) {
-  listMusic(s, count);
-  num = selectNum();
+    listMusic(s, count);
+    num = selectNum();
     if (num > 0 && num <= count) {
     deleteMusic(s, count, num);
     }
@@ -57,11 +51,22 @@ while (1)
   }
   
   else if (menu == 5 && count > 0) {
-  saveData(s, count); 
+   listMusic(s, count);
+    num = selectNum();
+    if (num > 0 && num <= count) {
+    updateMusic(s[num - 1]);
+    }
+    else
+    {
+    printf("잘못된 번호입니다.\n");
+      }  
   }
   
   else if (menu == 6 && count > 0){
-  searchMusic(s, count);
+    downMusic(s, count);
+  } 
+   else if (menu == 7 && count > 0){
+    listenMusic(s, count);
   } 
 }
   // 동적 메모리 해제
